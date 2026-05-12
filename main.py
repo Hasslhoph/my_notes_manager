6e2db5b2d98ac49ae9444dc4fdae4a6d094693f1
Alexandr Sinelnikov <hasslhoph@gmail.com>
2026-05-12 21:28:42 +0700

    Реализовал добавление и просмотр заметок

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 print("Добро пожаловать в Менеджер заметок!")
+print(" ")
+notes = []  # здесь будут храниться все заметки
+
+
+def add_note():
+    new_title = input("Введите заголовок заметки: ")
+    new_text = input("Введите текст заметки: ")
+    my_dict = {"title": new_title, "text": new_text}
+    notes.append(my_dict)
+    print("Заметка добавлена!")
 
 
 def show_menu():
     print("=== МЕНЕДЖЕР ЗАМЕТОК ===")
+    print(" ")
     print("1. Добавить заметку")
     print("2. Просмотреть все заметки")
     print("3. Выйти")
     print(" ")
-    print(" ")
+
+
     while True:
         try:
             choice = int(input("Выберите действие: "))
             if choice == 1:
-                print("Добавление заметки (пока не реализовано)")
+                add_note()
             elif choice == 2:
-                print("Список заметок (пока пуст)")
+                print(notes)
             elif choice == 3:
                 break
             else:
